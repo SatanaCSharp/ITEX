02625d9e29e170198d04b85be0144d9fe287ca9b
Andey Zakrevsky <zak111revsky@gmail.com>
2018-06-30 17:20:37 +0300

    Hello

--- resources/views/publicPart/manager/companies/index.blade.php
+++ resources/views/publicPart/manager/companies/index.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
 
-    {{--<div class="card rounded-circle">--}}
-        {{--<img class="card-img-top" {!! Html::image('images/company/logos/'.$company->title.'/'.$company->logo.'') !!}  alt="Card image" >--}}
-    {{--</div>--}}
-
-     {{--<h3>{{$company->title}}</h3>--}}
-     {{--<h3>{{substr($company->description,0,30)}}...</h3>--}}
-     {{--<h3>{{$company->contacts}}</h3>--}}
-     {{--<h3>{{ $company->location}}</h3>--}}
-
-    {{--<a href="{{url('/manager/companies/'.$company->id.'/events/create')}}">Create event</a>--}}
-
     <div class="jumbotron card_company">
         <div class="container-fluid">
             <div class="row">
--- resources/views/publicPart/manager/events/index.blade.php
+++ resources/views/publicPart/manager/events/index.blade.php
@@ -3,27 +3,58 @@
 @section('content')
 
     @forelse($events as $event)
-        <div class="card rounded-circle">
-            <img class="card-img-top"
-                 {!! Html::image('images/company/logos/'.$company->title.'/'.$company->logo.'') !!}  alt="Card image">
+
+
+        {{--<h3>{{$event->duration}} h</h3>--}}
+        {{--@if($event->state == 0)--}}
+            {{--<a href="{{url('/manager/events/'.$event->id.'/reports')}}">Report</a>--}}
+            {{--<a href="{{url('/manager/events/'.$event->id.'/reports/create')}}">Create report</a>--}}
+        {{--@endif--}}
+
+
+{{--########################--}}
+
+    <div class="jumbotron card_company">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="company-logo-manager">
+                        {!! Html::image('images/company/logos/'.$company->title.'/'.$company->logo.'') !!}
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <ul class="nav flex-column desc-list-company ">
+                        <li class="nav-item ">
+                            <h2> {{$event->title}}</h2>
+                        </li>
+                        <li class="nav-item">
+                            <p class="desc_exc">{{substr($event->description,0,30)}}... </p>
+                        </li>
+                    @if($event->state == 1)
+                        <li class="nav-item ">
+                            <p class="desc_exc ">In planning</p>
+                        </li>
+                    @else
+                        <li class="nav-item ">
+                            <p class="desc_exc ">Occured</p>
+                        </li>
+                    @endif
+                        <li class="nav-item">
+                            <p class="desc_exc">{{$event->duration}}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3" >
+                    @if($event->state == 0)
+                        <a class="btn btn-primary btn-event" href="{{url('/manager/events/'.$event->id.'/reports')}}">Report</a>
+                        <a class="btn btn-primary btn-event" href="{{url('/manager/events/'.$event->id.'/reports/create')}}">Create report</a>
+                    @endif
+                </div>
+            </div>
+
         </div>
-        <h3>{{$event->title}}</h3>
-        <h3>{{substr($event->description,0,30)}}...</h3>
-
-        <h3>@if($event->state == 1)
-                In planning
-            @else
-                Occured
-            @endif
-        </h3>
-
-        <h3>{{$event->duration}} h</h3>
-        @if($event->state == 0)
-            <a href="{{url('/manager/events/'.$event->id.'/reports')}}">Report</a>
-            <a href="{{url('/manager/events/'.$event->id.'/reports/create')}}">Create report</a>
-        @endif
+    </div>
     @empty
         <h3>There no excursions</h3>
     @endforelse
-
 @endsection
